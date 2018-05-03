cc684c26591eeed2ad1da312bb683cdb9733f5be
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-05-03 14:17:05 +0200

    user dao methods almost ready

--- src/main/java/com/codecool/web/dao/UserDao.java
+++ src/main/java/com/codecool/web/dao/UserDao.java
@@ -1,13 +1,14 @@
 package com.codecool.web.dao;
 
 
+import com.codecool.web.exceptions.NotFoundException;
 import com.codecool.web.model.User;
 
 import java.sql.SQLException;
 
 public interface UserDao {
 
-    User findByName(String name) throws SQLException;
+    User findByName(String name) throws SQLException, NotFoundException;
     User insertNewUser(String name, String password, String role) throws SQLException;
 
 }
