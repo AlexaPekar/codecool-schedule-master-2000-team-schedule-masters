9812952a7e5d24b001d5138b8c4eb26588ce26c0
peti120 <peti59550@gmail.com>
2018-05-03 14:07:39 +0200

    NotFoundException created

---
+++ src/main/java/com/codecool/web/exceptions/NotFoundException.java
@@ -0,0 +1,12 @@
+package com.codecool.web.exceptions;
+
+public class NotFoundException extends Exception {
+
+
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
