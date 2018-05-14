76cd8e5bd99861bb0fc60549326100b7e119af28
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-05-14 10:06:13 +0200

    Service for User implemented

--- src/main/java/com/codecool/web/service/UserService.java
+++ src/main/java/com/codecool/web/service/UserService.java
@@ -1,4 +1,15 @@
 package com.codecool.web.service;
 
+import com.codecool.web.exceptions.NotFoundException;
+import com.codecool.web.exceptions.ServiceException;
+import com.codecool.web.model.User;
+
+import java.sql.SQLException;
+
 public interface UserService {
+
+    User getByName(String name) throws NotFoundException, SQLException, ServiceException;
+
+    User addNewUser(String name,String password,String role) throws SQLException, ServiceException;
+
 }
--- src/main/java/com/codecool/web/service/simple/SimpleUserService.java
+++ src/main/java/com/codecool/web/service/simple/SimpleUserService.java
@@ -1,4 +1,39 @@
 package com.codecool.web.service.simple;
 
-public class SimpleUserService {
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.exceptions.EmptyFieldException;
+import com.codecool.web.exceptions.NotFoundException;
+import com.codecool.web.exceptions.ServiceException;
+import com.codecool.web.model.User;
+import com.codecool.web.service.UserService;
+
+import java.sql.SQLException;
+
+public class SimpleUserService implements UserService {
+
+    private final UserDao uD;
+
+    public SimpleUserService(UserDao uD) {
+        this.uD = uD;
+    }
+
+    @Override
+    public User getByName(String name) throws NotFoundException, SQLException, ServiceException {
+        if (name.equals("")) {
+            throw new ServiceException("You didn't enter a name");
+        }
+        if (uD.findByName(name) == null) {
+            throw new ServiceException("User not found");
+        }
+        return uD.findByName(name);
+    }
+
+    @Override
+    public User addNewUser(String name, String password, String role) throws SQLException, ServiceException {
+        if (name.equals("") || password.equals("") || role.equals("") ) {
+            throw new ServiceException(new EmptyFieldException("Fill all fields"));
+        }
+
+        return uD.insertNewUser(name,password,role);
+    }
 }
