53587dc62f9aacae14964b37a2f50395e6682601
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-05-16 14:03:37 +0200

    columns.js implemented

---
+++ src/main/webapp/columns.js
@@ -0,0 +1,57 @@
+
+
+function getScheduleColumns(scheduleId){
+    const link = "/schedule-masters/protected/columns/"+scheduleId;
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onColumnsReceived);
+    xhr.open('GET',link);
+    xhr.send();
+}
+
+function onColumnsReceived() {
+    const text = this.responseText;
+    const columns = JSON.parse(text);
+
+    const tableEl = document.createElement('table');
+    tableEl.appendChild(createColumnsTableBody(columns));
+
+    const scEl = document.getElementById('schedule');
+    scEl.appendChild(tableEl);
+}
+
+function createColumnsTableBody(columns) {
+    const tbodyEl = document.createElement('tbody');
+    const trEl1 = document.createElement('tr');
+
+    for (let i=0;i < columns.length; i++) {
+        const column = columns[i];
+
+        const colNameTdEl = document.createElement('td');
+        colNameTdEl.textContent = column.name;
+
+        trEl1.appendChild(colNameTdEl);
+    }
+
+    const trEl2 = document.createElement('tr');
+
+    for (let i = 0; i < columns.length; i++) {
+        const column = columns[i];
+
+        const divEl = document.createElement('div');
+        divEl.textContent = "insert slot";
+        divEl.id = i;
+
+        // creating id cell
+        const colTdEl = document.createElement('td');
+        colTdEl.appendChild(divEl);
+
+
+        trEl2.appendChild(colTdEl);
+
+    }
+
+    tbodyEl.appendChild(trEl1);
+    tbodyEl.appendChild(trEl2);
+
+    return tbodyEl;
+}
