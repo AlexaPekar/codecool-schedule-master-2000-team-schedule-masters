c47be0e6d634019c3538a50e2fecb12d33af9ca6
peti120 <peti59550@gmail.com>
2018-05-30 15:16:23 +0200

    task add to slot implemented in js

--- src/main/webapp/slots.js
+++ src/main/webapp/slots.js
@@ -1,3 +1,5 @@
+let selectedSlot;
+
 function getColumnSlots(columnId){
     const link = "/schedule-masters/protected/slots/"+columnId;
     const xhr = new XMLHttpRequest();
@@ -31,7 +33,7 @@ function createSlotsTableBody(slots) {
 
         const slotContentTdEl = document.createElement('td');
         const divEl = document.createElement('div');
-        divEl.id = 'slot' + slot.id;
+        divEl.id = 'slot'+slot.id;
         slotContentTdEl.addEventListener('click',onSlotClicked);
         slotContentTdEl.id = "slotcontent";
 
@@ -45,31 +47,32 @@ function createSlotsTableBody(slots) {
     return tbodyEl;
 }
 function onSlotClicked(){
+    if(this.textContent === "" || this.textContent === null){
+        const slotText = this.children[0].id;
+        selectedSlot = slotText.substring(slotText.indexOf("t")+1);
+        getTaskForUsers();
+        const lightbox = document.getElementById("slot-lightbox");
+        const slotForm = document.getElementById("slot-form");
+        removeAllChildren(slotForm);
+        const dimmer = document.createElement("div");
 
-    getTaskForUsers();
-    const lightbox = document.getElementById("slot-lightbox");
-    const slotForm = document.getElementById("slot-form");
-    removeAllChildren(slotForm);
-    const dimmer = document.createElement("div");
+        dimmer.id = "dimmer";
+        dimmer.style.width =  window.innerWidth + 'px';
+        dimmer.style.height = window.innerHeight + 'px';
+        dimmer.className = 'dimmer';
 
-    dimmer.id = "dimmer";
-    dimmer.style.width =  window.innerWidth + 'px';
-    dimmer.style.height = window.innerHeight + 'px';
-    dimmer.className = 'dimmer';
+        dimmer.onclick = function(){
+            document.body.removeChild(this);
+            lightbox.style.visibility = 'hidden';
+        }
 
-    dimmer.onclick = function(){
-        document.body.removeChild(this);
-        lightbox.style.visibility = 'hidden';
+
+        document.body.appendChild(dimmer);
+
+        lightbox.style.visibility = 'visible';
+        lightbox.style.top = window.innerHeight/2 - 50 + 'px';
+        lightbox.style.left = window.innerWidth/2 - 100 + 'px';
     }
-
-
-    document.body.appendChild(dimmer);
-
-    lightbox.style.visibility = 'visible';
-    lightbox.style.top = window.innerHeight/2 - 50 + 'px';
-    lightbox.style.left = window.innerWidth/2 - 100 + 'px';
-
-
 }
 function getTaskForUsers(){
     const xhr = new XMLHttpRequest();
@@ -89,6 +92,7 @@ function createTaskSelectionForElement(tasks){
     const el = document.getElementById('slot-form');
     const selectDivEl = document.createElement('div');
     const selectEl = document.createElement('select');
+    selectEl.id = "task-selection";
     for (let i=0;i < tasks.length; i++) {
         const task = tasks[i];
         const optionEl = document.createElement('option');
@@ -100,7 +104,15 @@ function createTaskSelectionForElement(tasks){
     el.appendChild(selectDivEl);
 
 }
-
 function onSlotSaveClicked(){
-    console.log("nincskészgec");
+    const taskSelectEl = document.getElementById("task-selection");
+    const task = taskSelectEl.options[taskSelectEl.selectedIndex].value;
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('error', onNetworkError);
+    xhr.addEventListener('load', onScheduleRefresh);
+    xhr.open('POST', 'protected/addtasktoslot?scheduleId='+currentSchedule+"&slotId="+selectedSlot+"&taskId="+task);
+    xhr.send();
+
+    document.getElementById("dimmer").remove();
+    document.getElementById("slot-lightbox").style.visibility = "hidden";
 }
