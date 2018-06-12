744bb94ef948df6851ee5f4b7b7a1da296dd9196
AlexaPekar <missdreambelle@gmail.com>
2018-06-12 09:37:08 +0200

    Create new method in ScheduleService interface

--- src/main/java/com/codecool/web/service/ScheduleService.java
+++ src/main/java/com/codecool/web/service/ScheduleService.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface ScheduleService {
     Schedule getById(int id) throws SQLException, ServiceException;
+    List<Schedule>
     Schedule addSchedule(int userId,String name,int amountOfColumns) throws EmptyFieldException, ServiceException, SQLException;
     void editName(int id,String name) throws SQLException, ServiceException, EmptyFieldException;
     List<Schedule> getSchedulesByUserId(int userId) throws SQLException;
